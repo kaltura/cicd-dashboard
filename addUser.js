865184c8d44b16121ea50075470f862507beecec
Jonathan Kanarek <jonathan.amit-kanarek@Kaltura.com>
2019-03-19 18:16:56 +0200

    script to add user

--- addUser.js
+++ addUser.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 
-const config = JSON.parse(fs.readFileSync('config/config.json'));
-const logger = console;
+const email = process.argv[2];
+const username = process.argv[3];
+const role = process.argv[4];
+const url = process.argv[5];
 
-const model = require('./lib/model')(logger, config);
-model.User.Add("admin@kaltura.com", "Kaltura Admin", "123456", "Administrator")
-.then(user => console.log(user))
+var modules = {
+    logger: console,
+    config: JSON.parse(fs.readFileSync('config/config.json')),
+    permissions: JSON.parse(fs.readFileSync('config/permissions.json')),
+}
+modules.mail = require('./lib/mail')(modules);
+modules.model = require('./lib/model')(modules);
+
+
+const password = Math.random().toString(36).slice(-8);
+const updateInfo = `${url}#update-info`;
+modules.model.User.Add(email, username, password, role)
+.then(user => {
+    modules.mail.send(email, 'CI/CD Dashboard Registration Approved', 'register-approved', {password: password, updateInfo: updateInfo})
+    .then(process.exit, process.exit);
+}, process.exit);
